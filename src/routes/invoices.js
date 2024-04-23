1a8fe2e5ff8322095d9ea239937290c18697a06e
tisanbako <bakotisan@gmail.com>
2024-04-23 19:09:16 -0400

    updating  keys

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -2,7 +2,7 @@ export default function Invoices() {
     return (
         <main style={{ padding: "1rem 0" }}>
             <h2>Invoices v3</h2>
-            <h4>Tisan is here too</h4>
+            <h4>Tisan is here also</h4>
         </main>
     );
 }
