16f6034bbdb0947838fe414979283584c00e0525
tisanbako <bakotisan@gmail.com>
2024-04-27 21:02:38 -0400

    pushing files

--- src/routes/expenses.js
+++ src/routes/expenses.js
@@ -2,7 +2,7 @@ export default function Expenses() {
     return (
         <main style={{ padding: "1rem 0" }}>
             <h2>Expenses</h2>
-            <h4>Tisan is here again</h4>
+            <h4>Tisan is here again o </h4>
         </main>
     );
 }
